66cddfb6ecc3a4b8d6845f79c5302a39528226c4
enricobarone761 <enribarone01+github@gmail.com>
2025-10-24 09:50:45 +0200

    primo commit

---
+++ Esercitazione1/src/main/java/it/unipa/wsda/esercitazione1/Esercitazione1Application.java
@@ -0,0 +1,13 @@
+package it.unipa.wsda.esercitazione1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercitazione1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Esercitazione1Application.class, args);
+    }
+
+}
