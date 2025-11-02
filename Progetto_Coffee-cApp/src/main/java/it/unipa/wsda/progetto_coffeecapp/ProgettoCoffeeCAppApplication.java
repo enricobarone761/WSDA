c084682a9bf3ea7e9d646e26d569557e00ae3591
enricobarone761 <enribarone01+github@gmail.com>
2025-11-02 19:09:07 +0100

    oook

---
+++ Progetto_Coffee-cApp/src/main/java/it/unipa/wsda/progetto_coffeecapp/ProgettoCoffeeCAppApplication.java
@@ -0,0 +1,13 @@
+package it.unipa.wsda.progetto_coffeecapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoCoffeeCAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoCoffeeCAppApplication.class, args);
+    }
+
+}
---
+++ Progetto_Coffee-cApp/src/test/java/it/unipa/wsda/progetto_coffeecapp/ProgettoCoffeeCAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipa.wsda.progetto_coffeecapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoCoffeeCAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
