78fefd45b3a46a1b6043ce377a2fc8d5967d9be9
enricobarone761 <enribarone01+github@gmail.com>
2025-11-26 22:38:02 +0100

    Refactor manutentori management: update button IDs, streamline HTML structure, and implement add/remove functionality

--- Progetto_Coffee-cApp/src/main/resources/static/Gestore_sistema/gestore_sistema.js
+++ Progetto_Coffee-cApp/src/main/resources/static/Gestore_sistema/gestore_sistema.js
@@ -4,7 +4,7 @@ fetch('../XML/addetti_manutenzione.xml')
     .then(str => {
         xmlDoc = new DOMParser().parseFromString(str, "application/xml");
     })
-    .then(pippo)
+    .then(carica_addetti)
     .catch(errore => console.log(errore));
 
 
@@ -14,31 +14,52 @@ function costruttore_addetto(foo){
         'cognome': foo.querySelector('cognome').innerHTML,
         'email': foo.querySelector('email').innerHTML
     }
-
     return addetto
 }
 
-function carica_manutentori(addetto){
+function aggiungi_html_addetto(addetto){
 
     const div = document.createElement('div')
+    div.classList.add('manutentore-card') //ho messo questo per evitare che la card manutentore sia ulteruormente wrappata da un altro div
     div.innerHTML =
-        `<div class="manutentore-card">
-            <div class="manutentore-info">
+        `<div class="manutentore-info">
                 <div class="manutentore-nome">${addetto.nome} ${addetto.cognome}</div>
                 <div class="manutentore-email">${addetto.email}</div>
-            </div>
-            <button class="btn-piccolo btn-danger">Rimuovi</button>
-         </div>`;
+        </div>
+        <button class="btn-piccolo btn-danger rmv-addetto">Rimuovi</button>`;
+
+    //LOGICA PULSANTE RIMUOVI
+    div.querySelector('.rmv-addetto').addEventListener('click', listener => {
+        alert('Hai rimosso ' + addetto.nome + ' ' + addetto.cognome)
+        div.closest('.manutentore-card').remove();
+    })
 
     document.querySelector('.manutentori-list').appendChild(div)
     console.log(addetto)
 
 }
 
-function pippo(){
+function carica_addetti(){
     const addetti = xmlDoc.getElementsByTagName('addetto');
 
     for( let addetto of addetti ) {
-        carica_manutentori( costruttore_addetto(addetto) )
+        aggiungi_html_addetto( costruttore_addetto(addetto) )
     }
 }
+
+
+//LOGICA PULSANTE AGGIUNGI ADDETTO
+document.querySelector('#agg-add').addEventListener('click' , listener=>{
+    listener.preventDefault()
+    const nuovo_addetto ={
+        'nome': document.querySelector('#addetto-nome').value,
+        'cognome': document.querySelector('#addetto-cognome').value,
+        'email': document.querySelector('#addetto-email').value
+    }
+    if(document.querySelector('form').reportValidity()){
+        aggiungi_html_addetto(nuovo_addetto)
+    }
+
+})
+
+
