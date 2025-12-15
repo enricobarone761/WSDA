fd612ec383a66a9cc520dc77fbcd675e86fccc6c
Enrico Barone <enribarone01+github@gmail.com>
2025-12-15 21:23:48 +0100

    Delete heartbeat_service/.idea directory

--- heartbeat_service/.idea/queries/Query.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE utenti (
-                        id INT PRIMARY KEY AUTO_INCREMENT,
-                        nome VARCHAR(50) NOT NULL,
-                        cognome VARCHAR(50) NOT NULL,
-                        email VARCHAR(100) UNIQUE NOT NULL,
-                        data_nascita DATE,
-                        attivo BOOLEAN DEFAULT TRUE,
-                        data_creazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
