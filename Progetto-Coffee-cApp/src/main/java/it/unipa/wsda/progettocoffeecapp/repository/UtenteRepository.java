b716cada3b61a6f9aa1894345dbd64cdbbfde5ea
enricobarone761 <enribarone01+github@gmail.com>
2025-12-23 14:07:52 +0100

    Remove unused JpaRepository imports from DistributoreRepository and UtenteRepository

--- Progetto-Coffee-cApp/src/main/java/it/unipa/wsda/progettocoffeecapp/repository/UtenteRepository.java
+++ Progetto-Coffee-cApp/src/main/java/it/unipa/wsda/progettocoffeecapp/repository/UtenteRepository.java
@@ -1,7 +1,6 @@
 package it.unipa.wsda.progettocoffeecapp.repository;
 
 import it.unipa.wsda.progettocoffeecapp.model.Utente;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
