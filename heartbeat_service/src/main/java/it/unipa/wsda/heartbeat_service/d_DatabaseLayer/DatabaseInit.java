c2b48caee296edf8ea43da9b0d0e1c4104097b7c
enricobarone761 <enribarone01+github@gmail.com>
2025-12-29 16:07:58 +0100

    Rename CreazioneDistributori_info_db to DatabaseInit for clarity and consistency

--- heartbeat_service/src/main/java/it/unipa/wsda/heartbeat_service/d_DatabaseLayer/DatabaseInit.java
+++ heartbeat_service/src/main/java/it/unipa/wsda/heartbeat_service/d_DatabaseLayer/DatabaseInit.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class CreazioneDistributori_info_db {
+public class DatabaseInit {
     public static void main() {
         try (Connection conn = ConnessioneDistributore.getConnection();
              Statement stmt = conn.createStatement()) {
