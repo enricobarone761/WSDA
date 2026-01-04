c8f238b41767922f5ae5463855daa1496756f36e
enricobarone761 <enribarone01+github@gmail.com>
2026-01-04 19:43:18 +0100

    Remove unnecessary SSL parameters from database connection URLs in application.properties and ConnessioneDistributore.java

--- heartbeat_service/src/main/java/it/unipa/wsda/heartbeat_service/d_DatabaseLayer/ConnessioneDistributore.java
+++ heartbeat_service/src/main/java/it/unipa/wsda/heartbeat_service/d_DatabaseLayer/ConnessioneDistributore.java
@@ -9,7 +9,7 @@ public class ConnessioneDistributore {
 //    private static final String USER = "root";
 //    private static final String PASSWORD = "1234567890";
 
-    private static final String URL = "jdbc:mysql://coffee-capp-132184a6-coffee-capp-0176.g.aivencloud.com:15746/distributori_info?useSSL=true&trustServerCertificate=true";
+    private static final String URL = "jdbc:mysql://coffee-capp-132184a6-coffee-capp-0176.g.aivencloud.com:15746/distributori_info";
     private static final String USER = "avnadmin";
     private static final String PASSWORD = "AVNS_OuZduaX3vxn1_XlG49y";
 
