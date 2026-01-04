ae21560a73c4edb7562f19c7da143da247204db8
enricobarone761 <enribarone01+github@gmail.com>
2026-01-04 19:23:27 +0100

    Aggiunto db remoto

--- heartbeat_service/src/main/java/it/unipa/wsda/heartbeat_service/d_DatabaseLayer/ConnessioneDistributore.java
+++ heartbeat_service/src/main/java/it/unipa/wsda/heartbeat_service/d_DatabaseLayer/ConnessioneDistributore.java
@@ -5,9 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnessioneDistributore {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/distributori_info";
-    private static final String USER = "root";
-    private static final String PASSWORD = "1234567890";
+//    private static final String URL = "jdbc:mysql://127.0.0.1:3306/distributori_info";
+//    private static final String USER = "root";
+//    private static final String PASSWORD = "1234567890";
+
+    private static final String URL = "jdbc:mysql://coffee-capp-132184a6-coffee-capp-0176.g.aivencloud.com:15746/distributori_info?useSSL=true&trustServerCertificate=true";
+    private static final String USER = "avnadmin";
+    private static final String PASSWORD = "AVNS_OuZduaX3vxn1_XlG49y";
 
     public static Connection getConnection() throws SQLException {
 
