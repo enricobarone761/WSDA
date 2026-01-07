900b0025c3dc568122b9a1fb57517b31cf414f5d
enricobarone761 <enribarone01+github@gmail.com>
2026-01-07 23:32:04 +0100

    Refactor GestoreLoginController to remove unused authentication dependencies and streamline login handling

--- Progetto-Coffee-cApp/src/main/java/it/unipa/wsda/progettocoffeecapp/controller/gestoreSistema/GestoreLoginController.java
+++ Progetto-Coffee-cApp/src/main/java/it/unipa/wsda/progettocoffeecapp/controller/gestoreSistema/GestoreLoginController.java
@@ -1,32 +1,13 @@
 package it.unipa.wsda.progettocoffeecapp.controller.gestoreSistema;
 
-import it.unipa.wsda.progettocoffeecapp.model.Utente;
-import it.unipa.wsda.progettocoffeecapp.service.UtenteService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Optional;
-
 @Controller
 public class GestoreLoginController {
-
-    private final AuthenticationManager authenticationManager;
-
-    public GestoreLoginController(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
-    }
 
     @PostMapping("/login-gestore")
     public String login(@RequestParam String username,
