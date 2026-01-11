5dd5528aeff7bd6af891f8e861f57aa07646e8a2
enricobarone761 <enribarone01+github@gmail.com>
2026-01-11 19:13:03 +0100

    Remove unused Java version property from pom.xml

--- Progetto-Coffee-cApp/src/main/java/it/unipa/wsda/progettocoffeecapp/config/SvuotaTabellaConnessioneAvvio.java
+++ Progetto-Coffee-cApp/src/main/java/it/unipa/wsda/progettocoffeecapp/config/SvuotaTabellaConnessioneAvvio.java
@@ -6,14 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-//questa classe svuota la tabella Connessione ad ogni avvio di spring TODO
+//questa classe svuota la tabella Connessione ad ogni avvio di spring
+//per evitare problemi di sincronizzazione con il frontend in caso di riavvio TODO
 
 @Configuration
 public class SvuotaTabellaConnessioneAvvio {
 
     @Bean
-    CommandLineRunner clearConnessioneTable(ConnessioneRepository connessioneRepository) {
-        return _ -> {
+    CommandLineRunner svuotaTabellaConnessione(ConnessioneRepository connessioneRepository) {
+        return e -> {
             connessioneRepository.deleteAll();
             System.out.println("Tabella Connessione svuotata");
         };
