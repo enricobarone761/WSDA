de7b0e77ed1eab30f5d2486ed24e67c72825188a
enricobarone761 <enribarone01+github@gmail.com>
2026-01-11 18:23:12 +0100

    Refactor SincronizzazioneDatabase to RestController and update heartbeat.js to use dynamic distributor ID

--- Progetto-Coffee-cApp/src/main/java/it/unipa/wsda/progettocoffeecapp/controller/SincronizzazioneDatabase.java
+++ Progetto-Coffee-cApp/src/main/java/it/unipa/wsda/progettocoffeecapp/controller/SincronizzazioneDatabase.java
@@ -2,16 +2,11 @@ package it.unipa.wsda.progettocoffeecapp.controller;
 
 import it.unipa.wsda.progettocoffeecapp.model.StatiDistributori;
 import it.unipa.wsda.progettocoffeecapp.service.DistributoreService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("/sync")
+@RestController
 public class SincronizzazioneDatabase {
 
     private final DistributoreService distributoreService;
@@ -19,8 +14,7 @@ public class SincronizzazioneDatabase {
         this.distributoreService = distributoreService;
     }
 
-    @PostMapping
-    @ResponseBody
+    @PostMapping("/sync")
     public void syncStatus(@RequestBody List<String> idsGuasti){
         for (String id : idsGuasti) {
             System.out.println("Aggiornamento ricevuto " + id);
--- Progetto-Coffee-cApp/src/main/resources/static/Interfaccia_distributore/heartbeat.js
+++ Progetto-Coffee-cApp/src/main/resources/static/Interfaccia_distributore/heartbeat.js
@@ -1,5 +1,5 @@
 function sendHeartbeat() {
-    const distributoreId = "CA001"; 
+    const distributoreId = document.querySelector(".dispenser-id").innerText;
 
     // La chiamata punta all'endpoint /heartbeat gestito dalla Servlet esterna
     fetch(`http://localhost:8081/heartbeat_service_war_exploded/heartbeat?id=${distributoreId}`, {
@@ -19,6 +19,4 @@ function sendHeartbeat() {
 
 // Invia l'heartbeat ogni 60 secondi
 setInterval(sendHeartbeat, 60000);
-
-// Invia un heartbeat iniziale al caricamento della pagina
 sendHeartbeat();
